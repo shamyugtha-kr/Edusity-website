77a47962328b44bcb85ae055dfbeda16c708a4d8
shamyugtha-kr <shamyugthakr@gmail.com>
2024-06-14 17:20:59 +0530

    sonar

--- viteapp/src/components/Hero/Hero.jsx
+++ viteapp/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className='hero'>
         <div className="hero-text">
             <h1> We Ensure Better Education For A Better World!</h1>
-            <p> Our cutting-edge curriculam is designed to empower students with knowledge, skills,and experiences needed to excel in dynamic field od education.</p>
+            <p> Our cutting-edge curriculam is designed to empower students with knowledge, skills,and experiences needed to excel in dynamic field of education.</p>
             <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
         </div>
     </div>
